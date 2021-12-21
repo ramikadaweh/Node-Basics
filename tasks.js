5e5915a4c1b21411d7e645de5611dca48993a621
ramikadaweh <ramikadaweh31@gmail.com>
2021-12-21 15:35:06 +0200

    extended hello

--- tasks.js
+++ tasks.js
@@ -37,8 +37,8 @@ function onDataReceived(text) {
   if (text === 'quit\n' || text === 'exit\n') {
     quit();
   }
-  else if(text === 'hello\n'){
-    hello();
+  else if(text.startsWith('hello')  ){
+    hello(text);
   }
   else if(text ==='help\n'){
     help();
@@ -66,8 +66,8 @@ function unknownCommand(c){
  *
  * @returns {void}
  */
-function hello(){
-  console.log('hello!')
+function hello(name){
+  console.log(name.trim()+"!")
 }
 
 
